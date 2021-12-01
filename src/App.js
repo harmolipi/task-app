0a526d76db60bfb828e0bb092d00a13c9a685162
harmolipi <nikob381@gmail.com>
2021-11-30 23:55:13 -0500

    Moved input logic to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React from 'react';
 import Overview from './components/Overview.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Overview />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTask: '',
+    };
+  }
+
+  addTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: [...this.state.tasks, this.state.newTask],
+    });
+    document.querySelector('input').value = '';
+  };
+
+  updateState = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.addTask}>
+          <input onChange={this.updateState} placeholder="New task" />
+          <button>Add</button>
+        </form>
+        <Overview tasks={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,33 +1,12 @@
 import React from 'react';
 
 class Overview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [],
-    };
-    this.addTask = this.addTask.bind(this);
-  }
-
-  addTask(e) {
-    e.preventDefault();
-    const newTask = document.querySelector('input').value;
-    this.setState({
-      tasks: [...this.state.tasks, newTask],
-    });
-    document.querySelector('input').value = '';
-  }
-
   render() {
     return (
       <div>
-        <form>
-          <input placeholder="New task" />
-          <button onClick={this.addTask}>Add</button>
-        </form>
         <ul>
-          {this.state.tasks.map((task) => {
-            return <li key={task}>{task}</li>;
+          {this.props.tasks.map((task, index) => {
+            return <li key={index}>{task}</li>;
           })}
         </ul>
       </div>
